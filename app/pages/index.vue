a5494448029fac9088b979b40f82fc659abdee38
Shenley <582783985@qq.com>
2025-10-18 16:21:34 +0800

    wait

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -45,7 +45,8 @@ const listRecommended = computed(() => sort(
 <PostSlide v-if="listRecommended.length && page === 1 && !category" :list="listRecommended" />
 
 <div class="post-list">
-	<div class="toolbar">
+	<div>暂时，等我弄完</div>
+	<!-- <div class="toolbar">
 		<div>
 			<WidgetHiddenToggle ref="hiddenToggleRef" />
 		</div>
@@ -69,7 +70,7 @@ const listRecommended = computed(() => sort(
 		/>
 	</TransitionGroup>
 
-	<ZPagination v-model="page" :total-pages />
+	<ZPagination v-model="page" :total-pages /> -->
 </div>
 </template>
 
