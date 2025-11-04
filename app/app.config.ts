538465bd386bb5ac35d7081b777a7c93ec635c87
Shenley <582783985@qq.com>
2025-11-04 11:26:47 +0800

    123

--- app/app.config.ts
+++ app/app.config.ts
@@ -101,12 +101,13 @@ export default defineAppConfig({
 		{
 			title: '',
 			items: [
-				{ icon: 'ph:files-bold', text: '日志', url: '/' },
+				{ icon: 'ph:files-bold', text: '存档', url: '/' },
 				{ icon: 'ph:link-bold', text: '链接', url: '/link' },
 				{ icon: 'ph:archive-bold', text: '归档', url: '/archive' },
 				{ icon: 'ph:game-controller-bold', text: '游戏', url: '/game' },
 				{ icon: 'ph:television-simple-bold', text: '番剧', url: '/anime' },
-				{ icon: 'ph:clock-bold', text: '片刻', url: '/moments' },
+				{ icon: 'ph:clock-bold', text: '片刻（没写完）', url: '/moments' },
+				{ icon: 'ph:info-bold', text: '关于我', url: '/about' },
 			],
 		},
 	] satisfies Nav,
---
+++ app/pages/about.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { data: postLink } = await useAsyncData('/about', () => queryCollection('content').path('/about').first())
+const appConfig = useAppConfig()
+useSeoMeta({
+	title: '关于',
+	ogType: 'profile',
+	description: `${appConfig.title}的关于页面，介绍了网站的相关信息和作者的背景。`,
+})
+</script>
+
+<template>
+<ContentRenderer
+	v-if="postLink"
+	:value="postLink"
+	class="article"
+/>
+</template>
